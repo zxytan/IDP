1fdc79d18b4fb4737576bd76ee788b532d1e1d2e
Philippa Nunn <phmn3@cam.ac.uk>
2018-11-14 10:37:54 +0000

    initial code for yellow response

--- Robot/Robot/responses.cpp
+++ Robot/Robot/responses.cpp
@@ -1,2 +1,16 @@
 #include "Robot.h"
 
+void Robot::yellow_response() {
+	delay(3000); //stationary wait for 3 seconds
+
+	//flash yellow LED for 2 seconds 
+	for (i=0; i<= 8; ++i)
+		digitalWrite(yellow_LED, HIGH);
+		delay(250);
+		digitalWrite(yellow_LED, LOW);
+		delay(250);
+	
+
+
+
+}
