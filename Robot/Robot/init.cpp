7ec9fcc2d902ae1fcabed50a457e3843473bcbc9
Zoe Tan <zoetanxy@gmail.com>
2018-11-14 10:22:20 +0000

    more files, class functions edit

---
+++ Robot/Robot/init.cpp
@@ -0,0 +1,6 @@
+#include "Robot.h"
+
+void Robot::leave_box() {
+	int time = 5; //test to see how long motors need to run for robot to get out of box
+	forward(time);
+}
--- Robot/Robot/responses.cpp
+++ Robot/Robot/responses.cpp
@@ -1,2 +1,10 @@
 #include "Robot.h"
 
+void Robot::wall_response() {
+	loop_count += 1;
+	float reading;
+	get_compass_reading(reading);
+	turn(y_0_wall, reading);
+
+
+}
