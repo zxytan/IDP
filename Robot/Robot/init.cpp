d64f45f7fe7031d160981dcba49bcbd1ae4a01f4
Philippa Nunn <phmn3@cam.ac.uk>
2018-11-14 11:00:47 +0000

    initialise indicator LEDs

--- Robot/Robot/init.cpp
+++ Robot/Robot/init.cpp
@@ -4,3 +4,11 @@ void Robot::leave_box() {
 	int dist = 5; //check distance to get out of box
 	forward(dist, 1);
 }
+
+//set up LEDs
+
+init yellow_LED = 7;
+init red_LED = 6;
+
+pinMode(yellow_LED, OUTPUT);
+pinMode(red_LED, OUTPUT);
