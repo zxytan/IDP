f3a9b00c22b0ed8e2d423153857d28acdd40fbe1
Zoe Tan <zoetanxy@gmail.com>
2018-11-15 09:47:01 +0000

    minor changes

--- src/Robot.h
+++ src/Robot.h
@@ -11,7 +11,7 @@
 #if defined(ARDUINO) && ARDUINO >= 100
 	#include "arduino.h"
 #else
-	#include "WProgram.h"
+	#include <WProgram.h>
 #endif
 
 class Robot {
--- src/responses.cpp
+++ src/responses.cpp
@@ -5,7 +5,7 @@ void Robot::yellow_response() {
 	delay(3000); //stationary wait for 3 seconds
 
 	//flash yellow LED for 2 seconds 
-	for (i=0; i<= 8; ++i)
+	for (int i=0; i<= 8; ++i)
 		digitalWrite(yellow_LED, HIGH);
 		delay(250);
 		digitalWrite(yellow_LED, LOW);
