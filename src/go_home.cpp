c17f762b651ea4f07a27dae47e234bd891b7a851
Philippa Nunn <phmn3@cam.ac.uk>
2018-11-15 10:40:38 +0000

    started go_home code

--- src/go_home.cpp
+++ src/go_home.cpp
@@ -1,9 +1,29 @@
 #include "Robot.h"
+/*	go_to_wall() code
+start_motor();
+get_prox_reading(p_reading);
+for (p_reading < y_max_length) { //put in numerical value for y_max_length
+	get_prox_reading(p_reading);
+	delay(1000);
+}
+stop_motor();
+
+*/
 
 void Robot::go_home() {
-	get_compass_reading(reading);
-	if reading == x_max_wall()
-		turn(y_0_wall, reading);
-		forward()
-
+	float c_reading;
+	float p_reading;
+	get_compass_reading(c_reading);
+	if c_reading == x_max_wall() {
+		turn(y_0_wall, c_reading);
+		go_to_wall()
+	}
+	else {
+		if loop_count == 8 {
+			turn(y_max_wall, c_reading);
+			go_to_wall();
+			get_compass_reading(c_reading);
+			turn(x_0_wall, c_reading);
+		}
+	}
 }
